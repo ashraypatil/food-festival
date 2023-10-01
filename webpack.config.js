d5c3a3c4c1d4535d3667f0eb9a3027b4bc436b73
Ashray <ashraypatil0415@gmail.com>
2023-10-01 00:28:47 -0700

    speed optimization complete

--- webpack.config.js
+++ webpack.config.js
@@ -24,5 +24,30 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: "[name].bundle.js",
   },
+  module: {
+  rules: [
+    {
+      test: /\.jpg$/i,
+      use: [
+          {
+            loader: 'file-loader',
+            options: {
+                esModule: false,
+                name (file) {
+                    return "[path][name].[ext]"
+                },
+                publicPath: function(url) {
+                    return url.replace("../", "/assets/")
+                }
+            }
+                         
+          },
+          {
+          loader: 'image-webpack-loader'
+        }
+     ]
+    }
+  ]
+},
   // ...other Webpack configuration options
 };
