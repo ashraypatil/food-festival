05e27ef02ca78405fcd6f94b461dd58026182411
Ashray <ashraypatil0415@gmail.com>
2023-09-30 18:10:12 -0700

    file loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 
 
 module.exports = {
   plugins: [ new webpack.ProvidePlugin({
     $: "jquery",
     jQuery: "jquery"
-  }),],  
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+  })
+],  
   mode: 'development', // Set the mode to 'development'
-  entry: './assets/js/script.js',
+  entry: {
+  app: "./assets/js/script.js",
+  events: "./assets/js/events.js",
+  schedule: "./assets/js/schedule.js",
+  tickets: "./assets/js/tickets.js"
+},
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: "[name].bundle.js",
   },
   // ...other Webpack configuration options
 };
