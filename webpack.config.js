b22f42f8cc66177db3fe122be48ab79c0e817e35
Ashray <ashraypatil0415@gmail.com>
2023-10-02 22:47:22 -0700

    module 18 & 19 changes still needed

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,47 @@
 const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-
+// Define a variable to toggle the BundleAnalyzerPlugin
+const isBundleAnalysisEnabled = process.env.ANALYZE_BUNDLE === 'true';
 
 module.exports = {
   plugins: [ new webpack.ProvidePlugin({
     $: "jquery",
     jQuery: "jquery"
   }),
-  new BundleAnalyzerPlugin({
-    analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-  })
+
+
+
+
+    new BundleAnalyzerPlugin({
+     analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    })
+
+  
 ],  
+
+new :WebpackPwaManifest({
+  name: "Food Event",
+  short_name: "Foodies",
+  description: "An app that allows you to view upcoming food events.",
+  start_url: "../index.html",
+  background_color: "#01579b",
+  theme_color: "#ffffff",
+  fingerprints: false,
+  inject: false,
+  icons: [{
+    src: path.resolve("assets/img/icons/icon-512x512.png"),
+    sizes: [96, 128, 192, 256, 384, 512],
+    destination: path.join("assets", "icons")
+  }]
+}),
+
+
   mode: 'development', // Set the mode to 'development'
+
+  
   entry: {
   app: "./assets/js/script.js",
   events: "./assets/js/events.js",
